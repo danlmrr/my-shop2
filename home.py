875c5f3eec6b1bba90af700633ab987ef3957305
Tatarnikov Danila <tatarnikovdanil49@gmail.com>
2023-02-03 01:23:38 +0300

    files add

---
+++ home.py
@@ -0,0 +1,26 @@
+# import time
+#
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# driver = webdriver.Chrome()
+# driver.maximize_window()
+#
+# driver.implicitly_wait(10)
+#
+# driver.get('https://practice.automationtesting.in/')
+#
+# driver.execute_script("window.scrollBy(0, 600);")
+# ruby = driver.find_element(By.CSS_SELECTOR, "[title='Selenium Ruby']").click()
+# reviews = driver.find_element(By.CLASS_NAME, "reviews_tab").click()
+# star = driver.find_element(By.CLASS_NAME, "star-5").click()
+# comments = driver.find_element(By.ID, "comment")
+# comments.send_keys("Nice book!")
+# name = driver.find_element(By.ID, "author")
+# name.send_keys("Alex")
+# email = driver.find_element(By.ID, "email")
+# email.send_keys("alex123@gmail.com")
+# sumbit = driver.find_element(By.CSS_SELECTOR, "[value='Submit']").click
+#
+########### Задание2
+
+
---
+++ shop.py
@@ -0,0 +1,150 @@
+# import time
+#
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# driver = webdriver.Chrome()
+# driver.maximize_window()
+# driver.implicitly_wait(5)
+#
+# driver.get('https://practice.automationtesting.in/')
+# my_account = driver.find_element(By.LINK_TEXT, "My Account").click()
+# username = driver.find_element(By.ID, "username")
+# username.send_keys("alex123@gmail.com")
+# password = driver.find_element(By.ID, "password")
+# password.send_keys("532Alex131")
+# login = driver.find_element(By.NAME, "login").click()
+# shop = driver.find_element(By.LINK_TEXT, "Shop").click()
+# book_html5_forms = driver.find_element(By.CSS_SELECTOR, "[src='https://practice.automationtesting.in/wp-content/uploads/2017/01/Mastering-HTML5-Forms-300x300.jpg']").click()
+#
+# html5_forms = driver.find_element(By.CLASS_NAME, "entry-title")
+# html5_forms_text = html5_forms.text
+# assert html5_forms_text == "HTML5 Forms"
+# if html5_forms_text == "HTML5 Forms":
+#     print("Название соответсвует")
+# import time
+import time
+
+################Задание 2
+
+# import time
+#
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# driver = webdriver.Chrome()
+# driver.maximize_window()
+# driver.implicitly_wait(5)
+#
+# driver.get('https://practice.automationtesting.in/')
+# my_account = driver.find_element(By.LINK_TEXT, "My Account").click()
+# username = driver.find_element(By.ID, "username")
+# username.send_keys("alex123@gmail.com")
+# password = driver.find_element(By.ID, "password")
+# password.send_keys("532Alex131")
+# login = driver.find_element(By.NAME, "login").click()
+# shop = driver.find_element(By.LINK_TEXT, "Shop").click()
+# html = driver.find_element(By.CSS_SELECTOR, "[href='https://practice.automationtesting.in/product-category/html/']").click()
+#
+# items = driver.find_elements(By.CSS_SELECTOR, "[class='count']")
+# if items == "(3)":
+#     print("3 товара")
+
+
+
+
+
+
+################### Задание 3
+
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# driver = webdriver.Chrome()
+# driver.maximize_window()
+# driver.implicitly_wait(5)
+#
+# driver.get('https://practice.automationtesting.in/')
+# my_account = driver.find_element(By.LINK_TEXT, "My Account").click()
+# username = driver.find_element(By.ID, "username")
+# username.send_keys("alex123@gmail.com")
+# password = driver.find_element(By.ID, "password")
+# password.send_keys("532Alex131")
+# login = driver.find_element(By.NAME, "login").click()
+# shop = driver.find_element(By.LINK_TEXT, "Shop").click()
+#
+# default = driver.find_element(By.CSS_SELECTOR, "[value='menu_order']")
+# default_text = default.text
+# assert default_text == "Default sorting"
+# if default_text == "Default sorting":
+#     print("Дефолт")
+# else:
+#     print("Неверно")
+#
+#
+# driver.find_element(By.NAME, "orderby").click()
+# driver.find_element(By.CSS_SELECTOR, "[value='price-desc']").click()
+#
+# driver.find_element(By.NAME, "orderby").click()
+# driver.find_element(By.CSS_SELECTOR, "[value='menu_order']").click()
+#
+# driver.find_element(By.NAME, "orderby").click()
+# driver.find_element(By.CSS_SELECTOR, "[value='price-desc']").click()
+
+# time.sleep(4)
+############# Задание 4
+
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
+#
+# driver = webdriver.Chrome()
+# driver.maximize_window()
+# driver.implicitly_wait(5)
+# driver.get('https://practice.automationtesting.in/')
+#
+#
+#
+# my_account = driver.find_element(By.LINK_TEXT, "My Account").click()
+# username = driver.find_element(By.ID, "username")
+# username.send_keys("alex123@gmail.com")
+# password = driver.find_element(By.ID, "password")
+# password.send_keys("532Alex131")
+# login = driver.find_element(By.NAME, "login").click()
+# shop = driver.find_element(By.LINK_TEXT, "Shop").click()
+#
+# android = driver.find_element(By.CSS_SELECTOR,"[href='https://practice.automationtesting.in/product/android-quick-start-guide/']" ).click()
+#
+# bop = driver.find_element(By.CSS_SELECTOR, " .price > del > span ")
+# bop_text = bop.text
+# bnp = driver.find_element(By.CSS_SELECTOR, " .price > ins > span ")
+# bnp_text = bnp.text
+#
+# assert bop_text == "₹600.00"
+# assert bnp_text == "₹450.00"
+#
+# book_cover = WebDriverWait(driver, 10).until(
+#     EC.element_to_be_clickable((By.CSS_SELECTOR, ".images")))
+# book_cover.click()
+#
+# book_cover_close = WebDriverWait(driver, 10).until(
+#     EC.element_to_be_clickable((By.CSS_SELECTOR, ".pp_close")))
+# book_cover_close.click()
+# driver.quit()
+############## Задание 5
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.implicitly_wait(5)
+driver.get('https://practice.automationtesting.in/')
+
+my_account = driver.find_element(By.LINK_TEXT, "My Account").click()
+username = driver.find_element(By.ID, "username")
+username.send_keys("alex123@gmail.com")
+password = driver.find_element(By.ID, "password")
+password.send_keys("532Alex131")
+login = driver.find_element(By.NAME, "login").click()
+shop = driver.find_element(By.LINK_TEXT, "Shop").click()
